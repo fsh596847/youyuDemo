610b568308cbe83a6e48bab31885bf06a52e4aac
fsh596847 <fsh596847@163.com>
2017-11-24 09:39:39 +0800

    身份证验证

--- consumerlibrary/src/main/java/consumer/fin/rskj/com/library/utils/Constants.java
+++ consumerlibrary/src/main/java/consumer/fin/rskj/com/library/utils/Constants.java
@@ -9,7 +9,9 @@ import android.os.Environment;
 public class Constants {
   public static final int TOAST_SHOW_POSITION = 1;//Toast 显示位置：0：顶部 1：中间 2：底部
 
-  // face++
+  /**
+   * face++ 人脸识别
+   */
   public static String cacheText = "livenessDemo_text";
   public static String cacheImage = "livenessDemo_image";
   public static String cacheVideo = "livenessDemo_video";
@@ -27,19 +29,48 @@ public class Constants {
   public static final String REQUEST_URL = BASE_URL + "/jsonHttpServlet";
   public static final String LOGIN_URL = "http://test.rskj99.com/rskj-core-api/member/login";
   public static final String SHAREDPREFERENCES_NAME = "xm_preference";
-  public static final String CHANNEL_NO = "3";// 渠道标识
-
-  public static final String TRANS_CODE_YZM = "M000010";// 获取验证码接口标识
-  public static final String TRANS_CODE_UPLOAD_FRONT_PICTURE = "M120003";// 客户身份证正面照片上传
-  public static final String TRANS_CODE_UPLOAD_BACK_PICTURE = "M120004";// 客户身份证fan面照片上传
+  /**
+   * 渠道标识
+   */
+  public static final String CHANNEL_NO = "3";
+  /**
+   * 获取验证码接口标识
+   */
+  public static final String TRANS_CODE_YZM = "M000010";
+  /**
+   * 客户身份证正面照片上传
+   */
+  public static final String TRANS_CODE_UPLOAD_FRONT_PICTURE = "M120003";
+  /**
+   * 客户身份证fan面照片上传
+   */
+  public static final String TRANS_CODE_UPLOAD_BACK_PICTURE = "M120004";
   /**
    * 身份信息更新接口
    */
   public static final String TRANS_CODE_M100122 = "M100122";
-  public static final String TRANS_CODE_UPLOAD_BASIC_INFO = "M010310";// 客户基本信息上传接口标识
-  public static final String TRANS_CODE_UPLOAD_ALL_CONTANTS_INFO = "S000010";// 客户通讯录信息上传接口标识
-  public static final String TRANS_CODE_QUERY_H5_URL = "S300002";// 查询H5URL接口
-  public static final String TRANS_CODE__CHECK_BANKCARD_IS_QINGYUAN = "S200005";// 判断是否为清远农商的银行卡接口
-  public static final String TRANS_CODE_FACE_CHECK = "M100107";// 活体检测接口
-  public static final String TRANS_CODE_BANK_CARD_CHECK_THREE = "M100109";// 银行卡号验证三接口
+  /**
+   * 客户基本信息上传接口标识
+   */
+  public static final String TRANS_CODE_UPLOAD_BASIC_INFO = "M010310";
+  /**
+   * 客户通讯录信息上传接口标识
+   */
+  public static final String TRANS_CODE_UPLOAD_ALL_CONTANTS_INFO = "S000010";
+  /**
+   * 查询H5URL接口
+   */
+  public static final String TRANS_CODE_QUERY_H5_URL = "S300002";
+  /**
+   * 判断是否为清远农商的银行卡接口
+   */
+  public static final String TRANS_CODE__CHECK_BANKCARD_IS_QINGYUAN = "S200005";
+  /**
+   * 活体检测接口
+   */
+  public static final String TRANS_CODE_FACE_CHECK = "M100107";
+  /**
+   * 银行卡号验证三接口
+   */
+  public static final String TRANS_CODE_BANK_CARD_CHECK_THREE = "M100109";
 }
