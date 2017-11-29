b1414c617166255a7ba71d73c86e3bf20241e6dd
fsh596847 <fsh596847@163.com>
2017-11-29 17:34:56 +0800

    修改布局

--- app/src/main/java/com/zhongan/demo/fragments/UnLoginFragment.java
+++ app/src/main/java/com/zhongan/demo/fragments/UnLoginFragment.java
@@ -55,8 +55,7 @@ public class UnLoginFragment extends HXBaseFragment {
 
     mBaseView.findViewById(R.id.loan_money).setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
-        Intent intent =
-            new Intent(getActivity(), consumer.fin.rskj.com.library.login.LoginActivity.class);
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
         startActivity(intent);
       }
     });
